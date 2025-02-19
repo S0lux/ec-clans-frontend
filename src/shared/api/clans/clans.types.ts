050ae9f85361d04fd0d6e85d60df12aa4494fae9
Solux <124425848+S0lux@users.noreply.github.com>
2025-02-18 19:55:17 -0500

    Removed erroneous import

--- src/shared/api/clans/clans.types.ts
+++ src/shared/api/clans/clans.types.ts
@@ -1,12 +1,10 @@
 import { z } from "zod";
 import {
   clanDtoSchema,
-  clanGuildDtoSchema,
   clansListDtoSchema,
   officializeClanDtoSchema,
 } from "./clans.dtos";
 
 export type OfficializeClanDto = z.infer<typeof officializeClanDtoSchema>;
-export type ClanGuildDto = z.infer<typeof clanGuildDtoSchema>;
 export type ClansListDto = z.infer<typeof clansListDtoSchema>;
 export type ClanDto = z.infer<typeof clanDtoSchema>;
